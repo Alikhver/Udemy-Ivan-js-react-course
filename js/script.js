87f2722c17c12bf44732cda813f13213207c38b5
Dzmitry Alikhver <dolihver@gmail.com>
2024-01-11 22:25:18 +0300

    Lecture 19

--- js/script.js
+++ js/script.js
@@ -1,4 +1,17 @@
 "use strict"
 
-const result = prompt("Bist du hier?", "J")
-console.log(result)
+let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?");
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+}
+
+let q1 = prompt("Один из последних просмотренных фильмов?", "11:14");
+let q2 = prompt("На сколько оцените его?");
+
+personalMovieDB.movies[q1] = q2;
+ 
+console.log(personalMovieDB)
