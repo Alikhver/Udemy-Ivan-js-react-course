1602f9cfa78eab5f3a077d531748706a077fcc96
Dzmitry Alikhver <dolihver@gmail.com>
2024-01-13 22:34:52 +0300

    replaced with fibonachi task

--- js/script.js
+++ js/script.js
@@ -1,49 +1,23 @@
 "use strict"
 
-// let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?");
-// const personalMovieDB = {
-//     count: numberOfFilms,
-//     movies: {},
-//     actors: {},
-//     genres: [],
-//     private: false
-// }
-
-// let q1 = prompt("Один из последних просмотренных фильмов?", "11:14");
-// let q2 = prompt("На сколько оцените его?");
-
-// personalMovieDB.movies[q1] = q2;
- 
-// console.log(personalMovieDB3)
-
-// first: for (let i = 0; i < 3; i++) {
-//     console.log(`F${i}`)
-//     for (let j = 0; j < 3; j++) {
-//         console.log(`S${j}`)
-//         for (let k = 0; k < 3;) {
-//             console.log(`T${k}`)
-//             if (k === 2) {continue first;}
-//             k++;
-//         }
-//     }
-//     console.log(`${str}\n`)
-// }
-
-const lines = 5;
-let result = '';
-// Проверяется именно переменная result, формируйте строку в ней
-let stars = 1;
-let spaces = 5;
-while (spaces > -1) {
-    for (let i = 0; i < spaces; i++) {
-        result += ' ';
+function fib(a) {
+    if (!Number.isInteger(a) || a == 0) {
+        return '';
+    } else if (a == '1') {
+        return '0';
     }
-    for (let i = 0; i < stars; i++) {
-        result += '*'
-    }
-    result += '\n'
-    stars += 2
-    spaces-- 
+    let res = '0 1',
+        prev = 0,
+        cur = 1;
+    return rec(res, prev, cur, a);
 }
 
-console.log(result)
+function rec(res, prev, cur, a) {
+    if (a == 2) {
+        return res;
+    }
+    res += ` ${prev + cur}`;
+    return rec(res, cur, prev + cur, --a);
+}
+
+console.log(fib(20))
